30fd584ad035a0153d79d7ce0f57d0fc51be5f0c
Sumit Mahor <sumitmahor13@gmail.com>
2024-08-11 01:10:42 +0530

    add s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='w-full border h-[4rem] flex justify-center items-center shadow-md fixed top-0 bg-white '>
       <header className='text-center' >
-        <h1 className='text-3xl font-bold uppercase'>Sumit's Blogs</h1>
+        <h1 className='text-3xl font-bold uppercase'>Sumitt's Blogs</h1>
       </header>
     </div>
   )
